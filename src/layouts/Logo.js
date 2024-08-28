b31ca2f6153768236b9d2c99a5f07760ba35a449
Harindu Nimshan <harindumedawatta@gmail.com>
2024-08-28 16:35:40 +0530

    updated the Header component

--- src/layouts/Logo.js
+++ src/layouts/Logo.js
@@ -1,10 +1,12 @@
-import { ReactComponent as LogoDark } from "../assets/images/logos/adminpro.svg";
+// import { ReactComponent as LogoDark } from "../assets/images/logos/adminpro.svg";
+import AppLogo from '../assets/images/logos/app-logo2.png';
+import '../assets/styles/Header.css';
 import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
     <Link to="/">
-      <LogoDark />
+      <img src={AppLogo} alt="Reefguard Logo" className="app-logo2" />
     </Link>
   );
 };
