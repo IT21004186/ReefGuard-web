6a468d6e22ae24846611701062ef283323de2aeb
Harindu Nimshan <harindumedawatta@gmail.com>
2024-08-28 20:34:13 +0530

    initial commit

--- src/components/seaWater-pollution/seawaterpollutionIdentifier.js
+++ src/components/seaWater-pollution/seawaterpollutionIdentifier.js
@@ -3,6 +3,12 @@ import React from "react";
 
 function PollutionPredictor() {
 
+    return (
+        <div>
+            <h1>Pollution Predictor Meter</h1>
+        </div>
+    );
+
 }
 
 export default PollutionPredictor;
