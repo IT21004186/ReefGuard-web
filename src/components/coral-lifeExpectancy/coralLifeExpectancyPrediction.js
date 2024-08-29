05d423db654a8fe18b8c55ae19615fdaca9b3c6d
banukayapa <it21002342@my.sliit.lk>
2024-08-29 13:44:21 +0530

    added the header and sidebar to water pollution and life expectancy .js files. CSS is to be coded

--- src/components/coral-lifeExpectancy/coralLifeExpectancyPrediction.js
+++ src/components/coral-lifeExpectancy/coralLifeExpectancyPrediction.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Sidebar from "../../layouts/Sidebar";
+import Header from "../../layouts/Header";
 
 function CoralLifeExpectancyPrediction(){
-
+    return (
+        <div className="">
+          <Header />
+          <div className="">
+            <Sidebar />
+          </div>
+        </div>
+      );
 }
 
 export default CoralLifeExpectancyPrediction;
--- src/components/seaWater-pollution/seawaterpollutionIdentifier.js
+++ src/components/seaWater-pollution/seawaterpollutionIdentifier.js
@@ -1,8 +1,16 @@
 import React from "react";
-
+import Sidebar from "../../layouts/Sidebar";
+import Header from "../../layouts/Header";
 
 function PollutionPredictor() {
-
+  return (
+    <div className="">
+      <Header />
+      <div className="">
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
 
 export default PollutionPredictor;
